28c1c974cb925ca5a9f576fbba199c05eb4e1b52
Mitchell Wintrow <90mitchwintrow@gmail.com>
2025-05-05 19:18:57 -0700

    Add /users/create route to cmd/api/main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"database/sql"
+	"fmt"
+	"github.com/mrrobotisreal/journey-app-server-refresh/internal/db"
+	handlers_users_create "github.com/mrrobotisreal/journey-app-server-refresh/internal/handlers/users/create"
+	"log"
+	"net/http"
+)
+
+func main() {
+	if err := db.InitDB(); err != nil {
+		log.Fatalf("Failed to init MySQL: %v", err)
+	}
+	defer func(DB *sql.DB) {
+		err := DB.Close()
+		if err != nil {
+			log.Fatalf("Failed to close MySQL database: %v", err)
+		}
+	}(db.DB)
+
+	http.HandleFunc("/users/create", handlers_users_create.CreateAccount)
+
+	fmt.Println("Server up and running on port 6913...")
+
+	if err := http.ListenAndServe(":6913", nil); err != nil {
+		log.Fatalf("Failed to start server on port 6913: %v", err)
+	}
+}
